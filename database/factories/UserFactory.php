985c1f6c3c0470511cfff587f84a99c1144f4440
Mitesh <mitesh>
2023-08-26 17:58:03 +0530

    Prepare user factory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,9 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
@@ -18,8 +15,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
+            'username' => fake()->userName(),
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => preg_replace('/@example\..*/', '@yopmail.com', fake()->unique()->safeEmail),
+            'mobile' => fake()->phoneNumber(),
+            'country_code' => '+91',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -28,8 +28,10 @@ class UserFactory extends Factory
 
     /**
      * Indicate that the model's email address should be unverified.
+     *
+     * @return static
      */
-    public function unverified(): static
+    public function unverified()
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
